fa35922d5032eed0c5bba5eafae96d8bdfcd0b42
Karl Entwistle <me@karlentwistle.com>
2023-02-13 22:08:20 +0000

    Extract helpers

--- spec/dyno_spec.rb
+++ spec/dyno_spec.rb
@@ -112,32 +112,4 @@ RSpec.describe DarkKnight::Dyno do
       'source' => 'web.1'
     }
   end
-
-  def stub_restart_request(app_id_or_name, dyno_id_or_name)
-    stub_request(:delete, restart_uri(app_id_or_name, dyno_id_or_name))
-      .with(headers: restart_headers)
-      .to_return(status: 202)
-  end
-
-  def expect_restart_request(app_id_or_name, dyno_id_or_name)
-    expect(a_request(:delete, restart_uri(app_id_or_name, dyno_id_or_name))
-      .with(headers: restart_headers))
-      .to have_been_made.once
-  end
-
-  def expect_no_request
-    expect(a_request(:any, 'api.heroku.com')).not_to have_been_made
-  end
-
-  def restart_headers
-    {
-      'Accept' => 'application/vnd.heroku+json; version=3',
-      'Content-Type' => 'application/json',
-      'Authorization' => 'Bearer 01234567-89ab-cdef-0123-456789abcdef'
-    }
-  end
-
-  def restart_uri(app_id_or_name, dyno_id_or_name)
-    "https://api.heroku.com/apps/#{app_id_or_name}/dynos/#{dyno_id_or_name}"
-  end
 end
