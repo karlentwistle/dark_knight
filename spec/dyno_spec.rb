831b3aa36bce18bb2c6a24e4846ec8ae1290bcf3
Karl Entwistle <me@karlentwistle.com>
2023-02-13 21:10:38 +0000

    Reword test signatures

--- spec/dyno_spec.rb
+++ spec/dyno_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe DarkKnight::Dyno do
   end
 
   describe '#restart_if_swapping' do
-    it 'issues http request to heroku api if memory_total exceeds memory_total' do
+    it 'issues http request to heroku api if memory total exceeds memory quota' do
       stub_restart_request('todo', 'web.1')
 
       subject = described_class.new(source: 'web.1', memory_quota: 512.00, memory_total: 513.5)
@@ -39,7 +39,7 @@ RSpec.describe DarkKnight::Dyno do
       expect_restart_request('todo', 'web.1')
     end
 
-    it 'does nothing if memory_total equals memory_total' do
+    it 'does nothing if memory total equals memory quota' do
       subject = described_class.new(source: 'web.1', memory_quota: 512.00, memory_total: 512.0)
 
       subject.restart_if_swapping
@@ -47,7 +47,7 @@ RSpec.describe DarkKnight::Dyno do
       expect_no_request
     end
 
-    it 'does nothing false if memory_total is lower than memory_total' do
+    it 'does nothing false if memory total is lower than memory quota' do
       subject = described_class.new(source: 'web.1', memory_quota: 512.00, memory_total: 256.0)
 
       subject.restart_if_swapping
