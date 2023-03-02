af9b9d297579371a50f6bb91a0d32b863a4b0f4c
Karl Entwistle <me@karlentwistle.com>
2023-03-02 10:12:31 +0000

    Avoid attempting to restart the same dyno twice

--- lib/source_restart_lock.rb
+++ lib/source_restart_lock.rb
@@ -4,21 +4,21 @@ module DarkKnight
   class SourceRestartLock < Sequel::Model
     set_primary_key [:source]
     unrestrict_primary_key
-    plugin :validation_helpers
     plugin :optimistic_locking
+    plugin :update_or_create
+    plugin :validation_helpers
 
-    def self.if_source_unlocked(source)
-      if (existing = find(source:))
-        if existing.expires_at < Time.now
-          begin
-            existing.update(expires_at: existing.fetch_expires_at)
-            yield existing
-          rescue Sequel::Plugins::OptimisticLocking::Error
-            # no yield
-          end
-        end
-      else
-        yield create(source:)
+    def self.source_unlocked?(source)
+      where(source:).where { expires_at > Time.now }.none?
+    end
+
+    def self.source_locked?(source)
+      !source_unlocked?(source)
+    end
+
+    def self.lock_source(source)
+      update_or_create(source:) do |lock|
+        lock.expires_at = lock.fetch_expires_at
       end
     end
 
@@ -26,11 +26,6 @@ module DarkKnight
       super
       validates_presence %i[source expires_at]
       validates_unique :source
-    end
-
-    def before_validation
-      self.expires_at ||= fetch_expires_at
-      super
     end
 
     def process_type
--- spec/source_restart_lock_spec.rb
+++ spec/source_restart_lock_spec.rb
@@ -3,29 +3,21 @@
 require 'spec_helper'
 
 RSpec.describe DarkKnight::SourceRestartLock do
-  describe '.if_source_unlocked' do
-    it 'yields block if no source restart lock exists' do
-      expect { |b| described_class.if_source_unlocked('web.1', &b) }.to yield_control
+  describe '.source_unlocked?' do
+    it 'returns true no source restart lock exists' do
+      expect(described_class.source_unlocked?('web.1')).to eql(true)
     end
 
-    it 'yields if source restart lock exists but the lock has expired' do
+    it 'returns true if source restart lock exists but the lock has expired' do
       described_class.create(source: 'web.1', expires_at: one_day_ago)
 
-      expect { |b| described_class.if_source_unlocked('web.1', &b) }.to yield_control
+      expect(described_class.source_unlocked?('web.1')).to eql(true)
     end
 
-    it 'doesnt yield block if source restart lock already exists and hasnt expired' do
+    it 'returns false if source restart lock already exists and hasnt expired' do
       described_class.create(source: 'web.1', expires_at: one_minute_from_now)
 
-      expect { |b| described_class.if_source_unlocked('web.1', &b) }.not_to yield_control
-    end
-
-    it 'doesnt yield block if Sequel::Plugins::OptimisticLocking::Error is thrown' do
-      dbl = double(expires_at: one_day_ago, fetch_expires_at: one_minute_from_now)
-      allow(dbl).to receive(:update).and_raise(Sequel::Plugins::OptimisticLocking::Error)
-      allow(described_class).to receive(:find).with(source: 'web.1').and_return(dbl)
-
-      expect { |b| described_class.if_source_unlocked('web.1', &b) }.not_to yield_control
+      expect(described_class.source_unlocked?('web.1')).to eql(false)
     end
   end
 
